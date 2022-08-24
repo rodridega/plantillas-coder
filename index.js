468cacbcdcd48c3923624b6fea1e4761bb5ebade
Rodrigo <rodridega@gmail.com>
2022-08-24 00:50:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const { router, productos } = require("./router/routes");
+
+const { engine } = require("express-handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static(__dirname + "/public"));
+
+/* 
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./Handlebars/views");
+ */
+app.set("views", "./Pug");
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("datos", {mensaje: "Hola"});
+});
+
+app.get("/productos", (req, res) => {
+  console.log(productos);
+  res.render("productos", { productos });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
